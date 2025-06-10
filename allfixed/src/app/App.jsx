d1e8738856569949789d4403ff2e1c5d353263ee
Gabriel Chalmers <gabrielchalmers170@gmail.com>
2025-06-10 18:47:50 +0200

    Initial commit

---
+++ allfixed/src/app/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import "./App.css";
+
+const App = () => {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Welcome to the AllFixed App</h1>
+        <p>This is a simple React application.</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ allfixed/src/app/header.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import "./header.css";
+
+const HeaderComponent = () => {
+    return (
+        <div className="headerComponent">
+            <h6>Logo</h6>
+            <h6>About</h6>
+            <h6>Services</h6>
+            <h6>Contact Us</h6>
+        </div>
+    )
+}
+
+export default HeaderComponent;
