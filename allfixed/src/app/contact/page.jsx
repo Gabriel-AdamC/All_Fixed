4ddcd7c03a9d9c2ea485373c53b2c7fbaacac0d7
Gabriel Chalmers <gabrielchalmers170@gmail.com>
2025-06-17 16:54:56 +0200

    worked on view bug for contact page background

--- allfixed/src/app/contact/page.jsx
+++ allfixed/src/app/contact/page.jsx
@@ -5,11 +5,9 @@ import "./page.css";
 const contact = () => {
     return (
         <div>
-            <p>
-                hi
-            </p>
-            <div className="containerImage">
-                <img src="/allFixedContactPage.png" alt="Contact" style={{ display: 'block', width: '100%' }} />
+            <div className="contactPage">
+                <div>Tetst</div>
+                
             </div>
             <FormContact />
         </div>
