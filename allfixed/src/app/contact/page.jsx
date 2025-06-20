00db3c506452ea4ff19558f20cefbff938bb7442
Gabriel Chalmers <gabrielchalmers170@gmail.com>
2025-06-20 19:05:27 +0200

    Worked on mobile styling of contact page

--- allfixed/src/app/contact/page.jsx
+++ allfixed/src/app/contact/page.jsx
@@ -6,10 +6,27 @@ const contact = () => {
     return (
         <div>
             <div className="contactPage">
-                <div>Tetst</div>
-                
+                <h1 className="heading">Contact Us</h1>
+                <div className="spacer"></div>
+            </div>
+            <div className="svg"></div>
+            <div className="text">
+                <h4>How Can We Help You?</h4>
+                <p>
+                    Send us some feedback or explain your problem to us for a quote
+                </p>
             </div>
             <FormContact />
+            <div className="options">
+                <div className="text">
+                    <h4>Call Us</h4>
+                    <p>Placeholder</p>
+                </div>
+                <div className="text">
+                    <h4>Email Us</h4>
+                    <p>Placeholder</p>
+                    </div>
+            </div>
         </div>
         
     )
